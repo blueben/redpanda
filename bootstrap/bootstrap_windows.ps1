d8ff86426cf8754e2eb19f38260e86f7a1c7e539
Benjamin Krueger <benjamin@seattlefenix.net>
2016-12-18 18:22:19 -0800

    Create bootstrap_windows.ps1

---
+++ bootstrap/bootstrap_windows.ps1
@@ -0,0 +1,18 @@
+# Setup my execution policy for both the 64 bit and 32 bit shells
+set-executionpolicy remotesigned
+start-job -runas32 {set-executionpolicy remotesigned} | receive-job -wait
+
+# Install the latest stable ChefDK
+invoke-restmethod 'https://omnitruck.chef.io/install.ps1' | iex
+install-project chefdk -verbose
+
+# Install Chocolatey
+invoke-expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+
+# Get a basic setup recipe
+invoke-restmethod '' | out-file -encoding ascii -filepath c:/bootstrap_windows.rb
+
+# Use Chef Apply to setup 
+chef-apply c:/bootstrap_windows.rb
+write-host "executed chef recipe c:/bootstrap_windows.rb"
